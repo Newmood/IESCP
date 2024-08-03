38c8a2e7abd126f309bb67c7b9e452b17ce33df7
Soumya M <96116313+Newmood@users.noreply.github.com>
2024-08-04 01:53:42 +0530

    feat(layout, /home): add complete homepage and layout.html with styling

--- app.py
+++ app.py
@@ -4,6 +4,39 @@ import requests as rq
 
 app = Flask(__name__)
 
+ads_posts= [
+     {
+          "author" : "Fell Technologies",
+          "title" : "Promotional content on Fell G-series",
+          "description" : "Talk about laptops in G-series for 1 min highlighting all features.",
+          "budget" : "INR 15,000",
+          "industry" : "Technology",
+          "date_posted" : "03 August 2024",
+          "end_date" : "10 August 2024",
+          "public" : True
+     },
+     {
+          "author" : "Babel FinCorp",
+          "title" : "B-express Credit Card",
+          "description" : "Talk about all features of B-express credit card (except interest rate)",
+          "budget" : "INR 15,000",
+          "industry" : "Banking",
+          "date_posted" : "03 August 2024",
+          "end_date" : "10 August 2024",
+          "public" : True
+     },
+     {
+          "author" : "Babel FinCorp",
+          "title" : "B-express Credit Card",
+          "description" : "Talk about all features of B-express credit card (except interest rate)",
+          "budget" : "INR 15,000",
+          "industry" : "Banking",
+          "date_posted" : "03 August 2024",
+          "end_date" : "10 August 2024",
+          "public" : True
+     }
+]
+
 
 @app.route("/")
 def landing():
@@ -11,7 +44,7 @@ def landing():
 
 @app.route("/home")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title="Home" ,ads_posts= ads_posts)
 
 if __name__ == "__main__":
         app.run(debug=True)
